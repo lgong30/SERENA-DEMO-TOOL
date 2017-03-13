5ba35cc1c186ee43fd34bc72ddcdab05d5cadaf2
Long Gong <gonglong.gatech@gmail.com>
2017-03-13 19:52:28 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+"""This is the setup file for the project. The standard setup rules apply:
+   python setup.py build
+   sudo python setup.py install
+"""
+#from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup
+
+am_description = (
+    u'autoMatching is a simple tool for demonstrate MERGE procedure of two'
+    u'matchings. Using it, you can easily generate or demonstrate examples'
+    u'for the MERGE of two matchings.')
+
+setup(
+    name=u'autoMatching',
+    version=u'2017.03',
+    url='',
+    license='',
+    author='longgong',
+    author_email='long.github@gmail.com',
+    description='Automatically demonstrate merge of two matchings',
+    long_description=am_description,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    scripts=[u'amctl'],
+    data_files=[
+        (u'.', [u'matching_template.tex', u'cycle_template_flexible.tex'])
+    ],
+    install_requires=frozenset([
+        u'Flask',
+        u'Flask-script',
+        u'numpy',
+        u'networkx',
+        u'Send2Trash',
+        u'Jinja2',
+    ])
+)
